9da5366774076e51349a5ade01bb6ca20e17ec69
Xavier Geerinck <thebillkidy@hotmail.com>
2014-01-08 16:39:57 +0100

    Cleanup core

--- src/zeus/core.js
+++ src/zeus/core.js
@@ -1,7 +1,6 @@
 'use strict';
 
 window.app = {};
-window.app.providers = {};
 
 /**
  * On INIT, load all the files
@@ -23,18 +22,7 @@ var loadConfig = function(configName, configKey, callback) {
     
     // Load the files
     require(configKey, function() {
-        
-        // For Each file, get the name and save the function
-//        configKey.forEach(function(filePath) {
-//            // Get the last part of the url, this is the file name
-//            var fileParts = filePath.split('/');
-//            var file = fileParts[fileParts.length - 1];
-//            
-//            // Save the file
-//            app[file] = new app[file]();
-//            
-//        });
-        
+        // Callback if done loading.
         return callback();
     });
 };
@@ -44,37 +32,8 @@ var triggerDoneLoading = function() {
     
     // When done loading the framework, call the onFrameworkInit hook
     for (var i in app.core) {
-        console.log(i);
         if (app.core[i].onFrameworkInit) {
-        app.core[i].onFrameworkInit();    
+            app.core[i].onFrameworkInit();    
         }
     };
-    
 };
-//
-//var loadConfig = function(configName, configKey, callback) {
-//    var array = {};
-//    console.log(configKey);
-//    // Load the specified configKey files
-//    require(configKey, function () {
-//        // For each file registered
-//        configKey.forEach(function(filePath) {
-//            console.log(filePath);
-//            // Split the path by / to get each folder/file
-//            var fileParts = filePath.split('/');
-//    
-//            // Get the file name from the path
-//            var file = fileParts[fileParts.length - 1];
-//
-//            if (!app[configName][file]) {
-//                array.push(new app[configName][file]());
-//            } else if(app.providers[file]) {
-//                app.providers[file] = app.providers[file]();
-//            }
-//        });
-//        
-//        console.log(app);
-//        console.log(array);
-//        return array;
-//    });
-//};
