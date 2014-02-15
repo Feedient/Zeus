9e4a8b6c6c459fdcfedf247ecb53c6faa1546e69
AdriVanHoudt <adri.van.houdt@gmail.com>
2014-02-15 21:10:30 +0100

    async yey

--- src/zeus/core.js
+++ src/zeus/core.js
@@ -5,29 +5,27 @@ $(function() {
 
 	var initialize = function() {
 		app.core.hooks.trigger('preloaded', function() {
-				//TODO nothing todo here, but hooks require callback function
-		});
-		
-		var loaders = [];
+			var loaders = [];
 
-		for (var i in app.config.autoLoad) {
-			(function(key, value) {
-				loaders.push(function(callback) {
-					if ($.isArray(value)) {
-						loadFiles(key, value, callback);
-					} else {
-						loadFiles(key, value.files, callback, !value.initialize);
-					}
+			for (var i in app.config.autoLoad) {
+				(function(key, value) {
+					loaders.push(function(callback) {
+						if ($.isArray(value)) {
+							loadFiles(key, value, callback);
+						} else {
+							loadFiles(key, value.files, callback, !value.initialize);
+						}
+					});
+				}(i, app.config.autoLoad[i]));
+			}
+
+			// Autoload all components
+			async.series(loaders, function() {
+				// All files have been autoloaded, give libraries opportunity to preload own assets
+				app.core.hooks.trigger('loaded', function() {
+					// All necessary assets have been preloaded
+					app.core.hooks.trigger('ready');
 				});
-			}(i, app.config.autoLoad[i]));
-		}
-
-		// Autoload all components
-		async.series(loaders, function() {
-			// All files have been autoloaded, give libraries opportunity to preload own assets
-			app.core.hooks.trigger('loaded', function() {
-				// All necessary assets have been preloaded
-				app.core.hooks.trigger('ready');
 			});
 		});
 	};
